f4ea3ac6e0919afb27983064799cf2fafb02acd6
Siddhartha Joshi <sidjos@gmail.com>
2016-02-07 03:06:39 -0600

    try loop

--- TestingPackage_2016/scripts/LOOP_RUN_POWER_TEST_sid.sh
+++ TestingPackage_2016/scripts/LOOP_RUN_POWER_TEST_sid.sh
@@ -4,7 +4,8 @@
 
 timestamp=$(date +DATE_%y_%m_%d_TIME_%H_%M_%S)
 
-log=log_RUN_POWER_TEST_sid.log
+log=full_log_RUN_POWER_TEST_sid.log
+greplog = log_RUN_POWER_TEST_sid.log
 
 if [ -f $log ]; then
    echo "File '$log' Exists -> creating copy of old file with timestamp appended and creating new log"
@@ -17,11 +18,13 @@ else
 fi
 
 
-output_dir=/home/sidjoshi/test_sid_2015/VipramMezzFirmware/TestingPackage/sid_output/loop_$timestamp
+output_dir=/home/sidjoshi/test_sid_2015/VipramMezzFirmware/TestingPackage_2016/output/loop_$timestamp
 mkdir $output_dir
 
 timestamp=$(date +DATE_%y_%m_%d_TIME_%H_%M_%S)
 echo $timestamp |tee -a $log
+
+# | grep 'REAL\|---\|Vpre bit\|Vdd bit\|Dvdd bit\|memoryBlocksNeeded\|average\|Info\|No \|missing' 
 
 
 echo "Starting test..." |tee -a $log
@@ -35,8 +38,7 @@ stress=10
 
 v_start=25
 v_step=1
-v_end=35
-
+v_end=28
 
 sleep 1s
 for volt in `seq $v_start $v_step $v_end`;
@@ -44,7 +46,7 @@ do
 timestamp=$(date +%T)
 echo $timestamp |tee -a $log
 echo "---Running at frequency $freq and all voltages at $volt"
-python RunPower.py -b --freq $freq --vprech $volt --vdd $volt --dvdd $volt --NStress $stress --odir $output_dir | grep 'REAL\|---\|Vpre bit\|Vdd bit\|Dvdd bit\|memoryBlocksNeeded\|average\|Info' | tee  -a $log
+python RunPower.py -b --freq $freq --vprech $volt --vdd $volt --dvdd $volt --NStress $stress --odir $output_dir | tee  -a $log
 timestamp=$(date +%T)
 echo $timestamp |tee -a $log
 sleep 1s
@@ -53,6 +55,10 @@ done
 mv $log $output_dir
 mv results_Match_DVDD_v5.txt $output_dir
 
+more log | grep 'REAL\|---\|Vpre bit\|Vdd bit\|Dvdd bit\|memoryBlocksNeeded\|average\|Info\|No \|missing' >> $greplog
+cp $greplog $output_dir
+
 echo "...Test End" |tee -a $log
 echo "exit"
+
 
